bc985c645ec2eb02e094b4c662bde92255523c79
Alexander Eugene Beeken and Sarah Joy Calpo <student@epicodus.com>
2015-02-19 12:26:00 -0800

    break when you add tasks

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,68 @@ var Task = {
 
 
 $(function() {
-  $("")
+  $("#anotherList").click(function() {
+    $(".listNames").append(
+      '<div class="nameListDiv">' +
+        '<label for="nameList">Name:</label>' +
+        '<input type="text" class="nameList"><br>' +
+      '</div>'
+    );
+  });
+
+  var currentList;
+
+  $("form#formList").submit(function(event) {
+    event.preventDefault();
+
+
+    $(".nameListDiv").each(function(){
+      var inputtedName = $(this).find('input.nameList').val();
+      var newList = Object.create(List);
+      newList.setName(inputtedName);
+
+      $("#listLists").append('<li class="listListClick" id="' + newList.getName() + '">' + newList.getName() + '</li>');
+
+
+      $(".listListClick").last().click(function() {
+        currentList = newList;
+        $("#Tasks h2").text(currentList.getName());
+        $("#listTasks").empty()
+        $("#listTasks").append(currentList.task[0]);
+        // currentList.getTasks().forEach( function(task){
+        //   $("#listTasks").append('<li class="listTask">' + task.getName() + "</li>");
+        // });
+
+      });
+
+      $("#formTask").submit(function(event) {
+        event.preventDefault();
+        var newTaskName = $('#nameTask').val();
+        var newTask = Object.create(Task);
+        newTask.setName(newTaskName);
+        currentList.addTask(newTask);
+        alert(newTask.getName() + currentList.getName());
+
+
+        $("#listTasks").empty()
+        currentList.getTasks().forEach( function(task){
+
+        $("#listTasks").append('<li class="listTask">' + task.getName() + "</li>");
+
+        });
+
+        $(".nameTaskDiv input").val("");
+        $(".taskNames").empty()
+      });
+
+    });
+
+    $(".nameListDiv input").val("");
+    $(".listNames").empty()
+
+
+  });
+
 
 
 });
